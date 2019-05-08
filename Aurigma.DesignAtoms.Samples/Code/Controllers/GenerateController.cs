9293272f63c8b1404ff67c4f7f6e0a0b0bbd2289
Max Smalekha <m.smalekha@aurigma.com>
2019-05-08 14:23:20 +0700

    add backend sample.

---
+++ Aurigma.DesignAtoms.Samples/Code/Controllers/GenerateController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using Aurigma.DesignAtoms.Canvas.Collection;
+using Aurigma.DesignAtoms.Common;
+using Aurigma.DesignAtoms.Model;
+using Aurigma.DesignAtoms.Model.Items;
+using Aurigma.DesignAtoms.Serialization;
+using Newtonsoft.Json;
+
+namespace Aurigma.DesignAtoms.Samples.Code.Controllers
+{
+    public class GenerateController : ApiController
+    {
+        private readonly ProductJsonConverter _productJsonConverter;
+
+        public GenerateController(ProductJsonConverter productJsonConverter)
+        {
+            _productJsonConverter = productJsonConverter;
+        }
+
+        [HttpGet]
+        public HttpResponseMessage DemoProduct()
+        {
+            var product = new Product
+            {
+                Surfaces =
+                {
+                    new Surface(491.5f, 667.2f)
+                    {
+                        PrintAreas = { new PrintArea(new RectangleF(0, 0, 419.5f, 595.2f))},
+                        Containers =
+                        {
+                            new SurfaceContainer(new Collection<BaseItem>
+                            {
+                                new PlainTextItem("Just about any kind\nof print product",new PointF(95.2f, 321.5f),"Montserrat-Bold",24),
+                                new BoundedTextItem("Many packaged web-to-print solutions on the market today work well enough, but they can also be rigid and prevent the customizability that many printers want. Customer's Canvas provides an opportunity for printers to get a solution that is better tailored to their internal workflows and give their customers a more unique and intuitive experience.",new RectangleF(94.8f, 372.15f, 301.5f, 197.1f),"Montserrat-Regular",15),
+                                new ImageItem(new GraphicsMill.Bitmap(System.Web.Hosting.HostingEnvironment.MapPath("~/assets/image.jpg")), new PointF(58.8f , 60) , 301.9f, 174.48f)
+                                
+                                
+                            })
+                            {
+                                Name = Utils.MainContainerName
+                            }
+                        }
+                    }
+                }
+            };
+
+            var productJson = JsonConvert.SerializeObject(product, _productJsonConverter);
+
+            //Deserialization sample
+            //var test = JsonConvert.DeserializeObject<Product>(productJson, _productJsonConverter);
+
+            return new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(productJson, Encoding.UTF8, "application/json")
+            };
+        }
+    }
+}
---
+++ Aurigma.DesignAtoms.Samples/Code/Controllers/RenderController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Http;
+using Aurigma.DesignAtoms.Configuration.RenderingConfig;
+using Aurigma.DesignAtoms.Model;
+using Aurigma.DesignAtoms.Rendering;
+using Aurigma.GraphicsMill.Codecs;
+
+namespace Aurigma.DesignAtoms.Samples.Code.Controllers
+{
+    public class RenderController : ApiController
+    {
+        private readonly IProductRenderer _productRenderer;
+
+
+        public class RequestData
+        {
+            public Product Product;
+        }
+
+        public RenderController(IProductRenderer productRenderer)
+        {
+            _productRenderer = productRenderer;
+        }
+
+        public HttpResponseMessage Post([FromBody] RequestData data, string format)
+        {
+            var config = RenderingConfig.GetDefault();
+
+            var filename = "result.pdf";
+
+            if (format.Equals("jpeg", StringComparison.InvariantCultureIgnoreCase) ||
+                format.Equals("jpg", StringComparison.InvariantCultureIgnoreCase))
+            {
+                config.DefaultHiResOuputRendering.FileFormat = FileFormat.Jpeg;
+                filename = "result.jpeg";
+            }
+
+
+            var memoryStream = new MemoryStream();
+            _productRenderer.RenderHiRes(memoryStream, data.Product, config);
+
+            memoryStream.Position = 0;
+
+            var response = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StreamContent(memoryStream)
+            };
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue(MimeMapping.GetMimeMapping(filename));
+            response.Headers.CacheControl = new CacheControlHeaderValue
+            {
+                Public = true,
+                MaxAge = TimeSpan.FromHours(12)
+            };
+            response.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment")
+            {
+                FileName = filename
+            };
+
+            return response;
+        }
+    }
+}
